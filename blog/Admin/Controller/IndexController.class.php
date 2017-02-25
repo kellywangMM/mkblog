182ec402d2f765b445c7f0a89517ca36c17e9818
太阳雨 <1104935178@qq.com>
2017-02-25 23:37:50 +0800

    update 整理侧栏导航
    整理侧栏导航

--- blog/Admin/Controller/IndexController.class.php
+++ blog/Admin/Controller/IndexController.class.php
@@ -3,6 +3,8 @@ namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends CommonController {
     public function index(){
-       $this->display('index');
+    	$userInfo=$_SESSION['Admin'];
+    	$this->assign('userInfo',$userInfo);
+        $this->display('index');
     }
 }
